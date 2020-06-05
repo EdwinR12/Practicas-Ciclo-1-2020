844274b745f53ef25b360774c937c098aafdba79
edwinmenjivar888@gmail.com <edwinmenjivar888@gmail.com>
2020-06-05 00:23:22 -0600

    Update-final

--- holamundo/src/main/java/com/edwin/holamundo/holamundo/Controlador.java
+++ holamundo/src/main/java/com/edwin/holamundo/holamundo/Controlador.java
@@ -1,13 +1,17 @@
 package com.edwin.holamundo.holamundo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controlador {
 
 	@GetMapping("/")
-	public String Inicial( ) {
+	public String Inicial( Model mol) {
+		
+		String mensaje = "Hola desde el controlador!!";
+		mol.addAttribute("Saludo", mensaje);
 		
 		return "index";
 		
