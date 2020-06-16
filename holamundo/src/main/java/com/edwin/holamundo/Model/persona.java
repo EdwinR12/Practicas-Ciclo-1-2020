3f9e5f4cf5873965ea8c6b21b4a498687f9804c0
edwinmenjivar888@gmail.com <edwinmenjivar888@gmail.com>
2020-06-16 00:27:02 -0600

    Actualizado--XDXD😀😁

---
+++ holamundo/src/main/java/com/edwin/holamundo/Model/persona.java
@@ -0,0 +1,34 @@
+package com.edwin.holamundo.Model;
+
+public class persona {
+	private String Nombre;
+	private String Apellido;
+	private String Telefono;
+	private String email;
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+}
--- holamundo/src/main/java/com/edwin/holamundo/holamundo/Controlador.java
+++ holamundo/src/main/java/com/edwin/holamundo/holamundo/Controlador.java
@@ -1,8 +1,14 @@
 package com.edwin.holamundo.holamundo;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.edwin.holamundo.Model.persona;
 
 @Controller
 public class Controlador {
@@ -10,8 +16,27 @@ public class Controlador {
 	@GetMapping("/")
 	public String Inicial( Model mol) {
 		
-		String mensaje = "Hola desde el controlador!!";
-		mol.addAttribute("Saludo", mensaje);
+		persona per = new persona();
+		per.setNombre("Edwin");
+		per.setApellido("Rivas");
+		per.setTelefono("7840-7015");
+	    per.setEmail("rm0483032018@unab.edu.sv");
+	    
+		persona per2 = new persona();
+		per2.setNombre("Oswaldo");
+		per2.setApellido("Menjivar");
+		per2.setTelefono("7890-6014");
+	    per2.setEmail("rm0483032020@unab.edu.sv");
+	    
+	  List personas = (List) Arrays.asList(per,per2);
+		
+		//String mensaje = "Hola ,Que tal!! jaja XDXD";
+		
+		//mol.addAttribute("Saludo", mensaje);
+		//mol.addAttribute("persona",per);
+		mol.addAttribute("personas",personas);
+	
+		
 		
 		return "index";
 		
